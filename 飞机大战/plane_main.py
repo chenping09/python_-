55a65d28037c859ac4339f6e16daeab430a93d3a
chenping <cp199009@gmail.com>
2025-12-07 14:30:34 +0900

    变更

--- 飞机大战/plane_main.py
+++ 飞机大战/plane_main.py
@@ -13,6 +13,7 @@ class PlaneGame(object):
         self.__create_sprites()
         # 4.设置定时器事件 创建敌机 1s
         pygame.time.set_timer(CREATE_ENEMY_EVENT,1000)
+        pygame.time.set_timer(HERO_FIRE_EVENT,500)
 
     def __create_sprites(self):
         # 创建背景精灵和精灵组
@@ -24,6 +25,10 @@ class PlaneGame(object):
 
         # 创建敌机的精灵组
         self.enemy_group =pygame.sprite.Group()
+
+        # 创建英雄的精灵和精灵组
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
 
     def start_game(self):
         print("游戏开始....")
@@ -47,20 +52,54 @@ class PlaneGame(object):
                 PlaneGame.__game_over()
                 # exit()
             elif event.type == CREATE_ENEMY_EVENT:
-                print("敌机出场...")
+                #print("敌机出场...")
                 # 创建敌机精灵
                 enemy = Enemy()
                 # 将敌机精灵添加大敌机精灵组
                 self.enemy_group.add(enemy)
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+
+            #elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+               # print("向右移动...")
+
+        # 使用键盘提供的方法获取键盘的按键 - 按键元组
+        keys_pressed = pygame.key.get_pressed()
+        # 判断元组中对应的按键索引值为 1
+        if keys_pressed[pygame.K_RIGHT]:
+            # print("持续向右移动...")
+            self.hero.speed = 2
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed = -2
+        else :
+            self.hero.speed = 0
 
     def __check_collide(self):
-        pass
+        # 1.子弹摧毁敌机
+        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
+        # 2.敌机撞毁英雄检测
+        enemies = pygame.sprite.spritecollide(self.hero,self.enemy_group, True)
+
+        # 判断列表是否有内容
+        if len(enemies)>0:
+            # 让英雄牺牲
+            self.hero.kill()
+            # 结束游戏
+            PlaneGame.__game_over()
+
     def __update_sprites(self):
         self.back_group.update()
         self.back_group.draw(self.screen)
 
         self.enemy_group.update()
         self.enemy_group.draw(self.screen)
+
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
+
     @staticmethod
     def __game_over():
         print("游戏结束")
